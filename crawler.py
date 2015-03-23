d43291dafcfcb37f33c07b9a2bc27c0e4b083f8c
Xu Liu <xu@pelotoncycle.com>
2015-03-22 21:04:07 -0400

    init crawler

---
+++ crawler.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import threading
+import urllib2
+import logging
+
+from bs4 import BeautifulSoup
+import redis
+
+from room import (DouyuRoom, ZhanqiRoom, HuyaRoom)
+
+r = redis.StrictRedis(host='pub-redis-11890.us-east-1-2.3.ec2.garantiadata.com', port=11890, db=0, password='123456')
+
+
+class Crawler():
+    name = 'base_crawler'
+
+    def store_to_redis(self):
+        pipe = r.pipeline()
+        for i in xrange(len(self.rooms)):
+            room = self.rooms[i]
+            id = 'dota:{platform}:{index}'.format(platform=self.name, index=i)
+            pipe.hmset(id, 
+                       {
+                        'img': room.img,
+                        'url': room.url,
+                        'name': room.name,
+                        'viewers_count': room.viewers_count,
+                        'owner': room.owner,
+                        'source': self.name,
+                        })
+        pipe.execute()
+
+
+
+
+class ZhanqiCrawl(Crawler):
+
+    name = u'zhanqi'
+
+    def crawl(self, url='http://www.zhanqi.tv/games/dota2', count=5):
+        content = urllib2.urlopen(url).read()
+        html = BeautifulSoup(content)
+        lis = html.find(id='hotList').find_all('li')
+        self.rooms = []
+        for i in xrange(count):
+            li = lis[i]
+            self.rooms.append(ZhanqiRoom(li))
+        self.store_to_redis()
+        return self.rooms
+
+
+
+class DouyuCrawl(Crawler):
+
+    name = u'douyu'
+
+    def crawl(self, url='http://www.douyutv.com/directory/game/DOTA2', count=5):
+        # content = urllib2.urlopen(url).read()
+        html = BeautifulSoup(content)
+        lis = html.find(id='item_data').find_all('li')
+        self.rooms = []
+        for i in xrange(count):
+            li = lis[i]
+            self.rooms.append(DouyuRoom(li))
+        self.store_to_redis()
+        return self.rooms
+
+class HuyaCrawl(Crawler):
+
+    name = u'huya'
+
+    def crawl(self, url='http://www.huya.com/g/dota2', count=5):
+        content = urllib2.urlopen(url).read()
+        html = BeautifulSoup(content)
+        lis = html.find(id='video-item-live').find_all('li')
+        self.rooms = []
+        for i in xrange(count):
+            li = lis[i]
+            self.rooms.append(HuyaRoom(li))
+        self.store_to_redis()
+        return self.rooms  
+
+
+
+
+
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='(%(threadName)-10s) %(message)s',
+                        )
+    rooms = []
+    crawlers = [ZhanqiCrawl(), DouyuCrawl(), HuyaCrawl()]
+
+    def worker(crawler):
+        """thread worker function"""
+        logging.debug('make request from' + crawler.name)
+        rooms.extend(crawler.crawl())
+        logging.debug('done')
+        return
+
+    for crawler in crawlers:
+        t = threading.Thread(target=worker, args=(crawler,))
+        t.start()
+
+    main_thread = threading.currentThread()
+    for t in threading.enumerate():
+        if t is main_thread:
+            continue
+        logging.debug('joining %s', t.getName())
+        t.join()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ room.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+class Room:
+
+    source = 'default source'
+
+    def __init__(self, item):
+        self.item = item
+        self.get_room_img()
+        self.get_room_url()
+        self.get_room_name()
+        self.get_room_owner()
+        self.get_room_viewers_count()
+
+
+class ZhanqiRoom(Room):
+
+    root = 'http://www.zhanqi.tv'
+    source = u'战旗'
+
+    def get_room_img(self):
+        self.img = self.item.find('img').get('src')
+        # print self.img
+
+    def get_room_url(self):
+        self.url = self.root + self.item.select('.info-area')[0].a['href']
+        # print self.url
+
+    def get_room_name(self):
+        self.name = self.item.select('.info-area')[0].a.string
+        # print self.name
+
+    def get_room_viewers_count(self):
+        self.viewers_count = self.item.span.span.string
+        # print self.viewers_count
+
+    def get_room_owner(self):
+        self.owner = self.item.select('.anchor')[0].string
+        # print self.owner
+
+class DouyuRoom(Room):
+
+    root = 'http://www.douyutv.com'
+    source = u'斗鱼'
+
+    def get_room_img(self):
+        self.img = self.item.img['data-original']
+
+    def get_room_url(self):
+        self.url = self.root + self.item.a['href']
+
+    def get_room_name(self):
+        self.name = self.item.h1.string
+
+    def get_room_viewers_count(self):
+        self.viewers_count = self.item.select('span.view')[0].string
+
+    def get_room_owner(self):
+        self.owner = self.item.select('span.nnt')[0].string
+
+class HuyaRoom(Room):
+
+    root = 'http://www.huya.com/'
+    source = u'虎牙'
+
+    def get_room_img(self):
+        self.img = self.item.img['src']
+
+    def get_room_url(self):
+        self.url = self.item.a['href']
+
+    def get_room_name(self):
+        self.name = self.item.h6.string.strip()
+
+    def get_room_viewers_count(self):
+        self.viewers_count = self.item.select('span.num')[0].contents[1][:-3]
+
+    def get_room_owner(self):
+        self.owner = self.item.p.string.strip()
